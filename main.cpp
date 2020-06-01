95f6d91b11efd4846669a93df4efef5270d350fc
Kangmin Lee <45871242+damin1009@users.noreply.github.com>
2020-06-01 15:06:26 +0900

    Git Action 테스트

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Git Action Test" << endl;
+  int a;
+  cin >> a;
+  for (int i = 0; i < a; i++) {
+    cout << "WOW";
+  }
+  cout << endl;
+  return 0;
+}
